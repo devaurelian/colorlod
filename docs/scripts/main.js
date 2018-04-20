6faf4f123026a34175c4c45de56f438cc56de15f
helloworldwriter <writer@helloworldwriter.com>
2018-04-20 20:09:44 +0300

    Add WCAG option buttons; cleanup code

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -9,52 +9,77 @@ import * as utils from "/scripts/utils.js";
 
 const [computedElem, ratioElem, lodElem] = document.querySelectorAll("#computed, #ratio, #lod");
 
-const wrapEl = document.getElementById("wrap");
 const colorInputEl = document.getElementById("colorInput");
-const resultEl = document.getElementById("result"),
-    errorEl = document.getElementById("error");
+const resultEl = document.getElementById("result");
+const errorEl = document.getElementById("error");
+const themeColorEl = document.querySelector("meta[name=theme-color]");
 
-
-const curWCAGContrast = utils.contrastRatios.AA_LARGE;
-
-console.log(computedElem);
+let curWCAGContrast = utils.contrastRatios.AA_LARGE;
+let compRGB = {};
 
 function setTransparent() {
-    wrapEl.style.backgroundColor = "transparent";
-    wrapEl.style.color = "black";
+    document.body.style.backgroundColor = "transparent";
+    document.body.classList.remove("body--light");
+
 }
 
-colorInputEl.addEventListener("input", event => {
+function updateContrast() {
+    // Calculate the contrast between the current color and white
+    let contrast = utils.getContrastRatio(compRGB, utils.whiteRGB);
+
+    // Check whether a light or a dark contrast color should be used
+    const useLight = contrast >= curWCAGContrast;
+
+    // Update the text color with the light (white) or dark (black) color
+    document.body.classList.toggle("body--light", useLight);
+
+    // If a darker foreground color should be used, update the contrast with black
+    if (!useLight) contrast = utils.getContrastRatio(compRGB, utils.blackRGB);
+
+    // Update the computed, contrast, and lod information
+    ratioElem.textContent = `${contrast.toFixed(2)}:1`;
+    ratioElem.dataset.pass = contrast >= curWCAGContrast;
+    lodElem.textContent = useLight ? "light" : "dark";
+}
+
+colorInputEl.addEventListener("input", () => {
 
     setTransparent();
-    wrapEl.style.backgroundColor = colorInputEl.value;
+    document.body.style.backgroundColor = colorInputEl.value;
 
-    const compColorString = window.getComputedStyle(wrapEl).getPropertyValue("background-color");
+    const compColorString = window.getComputedStyle(document.body).getPropertyValue("background-color");
     console.log("Computed background-color", compColorString);
-    const compRGB = utils.parseComputedRGBColor(compColorString);
+    compRGB = utils.parseComputedRGBColor(compColorString);
     console.log("Computed RGB", compRGB);
 
     if (compRGB) {
-        // Calculate the contrast between the current color and white
-        let contrast = utils.getContrastRatio(compRGB, utils.whiteRGB);
-
-        // Check whether a light or a dark contrast color should be used
-        const useLight = contrast >= curWCAGContrast;
-
-        // Update the text color with the light (white) or dark (black) color
-        wrapEl.style.color = useLight ? "white" : "black";
-
-        // If a darker foreground color should be used, update the contrast with black
-        if (!useLight) contrast = utils.getContrastRatio(compRGB, utils.blackRGB);
-
-        // Update the computed, contrast, and lod information
         computedElem.textContent = compColorString;
-        ratioElem.textContent = `${contrast.toFixed(2)}:1`;
-        lodElem.textContent = useLight ? "light" : "dark";
+        updateContrast();
     } else {
         setTransparent();
     }
 
+    document.body.classList.toggle("checkered", !compRGB);
+
+    // Update the theme-color meta tag to update the browser toolbar color (on browsers that support this feature)
+    themeColorEl.setAttribute("content", compRGB ? compColorString : "");
+
     resultEl.classList.toggle("invisible", !compRGB);
     errorEl.classList.toggle("invisible", compRGB);
+});
+
+document.getElementById("optionsBar").addEventListener("click", event => {
+    if (event.target.tagName.toUpperCase() === "BUTTON") {
+        const button = event.target;
+
+        // Clear the "on" flag on all buttons
+        [...button.parentNode.children].forEach(btn => delete btn.dataset.on);
+
+        // Turn "on" the clicked button
+        button.dataset.on = true;
+
+        // Update the current WCAG contrast and recalculate and display current contrast information
+        curWCAGContrast = utils.contrastRatios[button.dataset.contrast];
+        updateContrast();
+    }
 });
--- docs/scripts/utils.js
+++ docs/scripts/utils.js
@@ -5,12 +5,10 @@
 
 /* eslint-disable no-magic-numbers, require-jsdoc */
 
-const CONTRAST_LIGHT_ITEM_THRESHOLD = 3;
+export const whiteRGB = { red: 255, green: 255, blue: 255 };
+export const blackRGB = { red: 0, green: 0, blue: 0 };
 
-export const whiteRGB = {red: 255, green: 255, blue: 255};
-export const blackRGB = {red: 0, green: 0, blue: 0};
-
-export const contrastRatios = { AA_NORMAL: 4.5, AA_LARGE: 3.1, AAA_NORMAL: 7, AAA_LARGE: 4.5 };
+export const contrastRatios = { AA_NORMAL: 4.5, AA_LARGE: 3, AAA_NORMAL: 7, AAA_LARGE: 4.5 };
 
 /**
  * Parses an rgb(R, G, B) string and returns a color value.
@@ -24,24 +22,6 @@ export function parseComputedRGBColor(value) {
     const rgb = /rgb\(([\d]+),([\d]+),([\d]+)\)/.exec(rgbString);
     // Return an RGB Color object, or null if the conversion fails
     return rgb ? { red: Number(rgb[1]), green: Number(rgb[2]), blue: Number(rgb[3]) } : null;
-}
-
-/**
- * Calculates the contrast between the given color and white, using the algorithm provided by the WCAG v2 in
- * {@link http://www.w3.org/TR/WCAG20/#contrast-ratiodef}.
- * Based on {@link https://goo.gl/FUWAta Chromium ColorUtils Java class}.
- * @param {Object} rgb The input color as an RGB color object.
- * @returns {number} The contrast value.
- */
-export function getContrastForColor(rgb) {
-    let bgR = rgb.red / 255;
-    let bgG = rgb.green / 255;
-    let bgB = rgb.blue / 255;
-    bgR = bgR < 0.03928 ? bgR / 12.92 : Math.pow((bgR + 0.055) / 1.055, 2.4);
-    bgG = bgG < 0.03928 ? bgG / 12.92 : Math.pow((bgG + 0.055) / 1.055, 2.4);
-    bgB = bgB < 0.03928 ? bgB / 12.92 : Math.pow((bgB + 0.055) / 1.055, 2.4);
-    const bgL = (0.2126 * bgR) + (0.7152 * bgG) + (0.0722 * bgB);
-    return Math.abs(1.05 / (bgL + 0.05));
 }
 
 export function getLuminance(rgb) {
@@ -63,14 +43,4 @@ export function getContrastRatio(rgb1, rgb2) {
 
 export function isReadable(rgb1, rgb2, contrastRatio) {
     return getContrastRatio(rgb1, rgb2) >= contrastRatio;
-}
-
-/**
- * Check whether lighter or darker foreground text should be used depending on the given background color.
- * Based on {@link https://goo.gl/4Lfuh2 Chromium ColorUtils Java class}.
- * @param {Object} backgroundColor The background color value which is being queried.
- * @returns {boolean} Whether light colored elements should be used.
- */
-export function shouldUseLightForegroundOnBackground(backgroundColor) {
-    return getContrastForColor(backgroundColor) >= CONTRAST_LIGHT_ITEM_THRESHOLD;
 }
